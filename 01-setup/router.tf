4dd7a7e72b502f6401d0c5eb2552b6683b03069d
Matthew Ratzke <matt@degree9.io>
2022-08-19 14:57:56 -0600

    added named port for router

---
+++ 01-setup/router.tf
@@ -0,0 +1,11 @@
+resource "google_compute_instance_group_named_port" "router" {
+  for_each = {
+    for index, stage in var.demo_stages : stage.name => stage
+  }
+  
+  group = "google_container_cluster.${var.demo_name}-${each.value.name}.node_pool[0].instance_group_urls[0]"
+  zone = "us-central1-a"
+
+  name = var.router_port_name
+  port = var.router_port
+}
