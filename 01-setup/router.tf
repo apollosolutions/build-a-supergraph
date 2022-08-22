b130239d407c3ce1deab0ba743ed36893d6fcbd5
Matthew Ratzke <matt@degree9.io>
2022-08-22 14:55:24 -0600

    Update router.tf

--- 01-setup/router.tf
+++ 01-setup/router.tf
@@ -3,7 +3,7 @@ resource "google_compute_instance_group_named_port" "router" {
     for index, stage in var.demo_stages : stage.name => stage
   }
   
-  group = "google_container_cluster.${var.demo_name}-${each.value.name}.node_pool[0].instance_group_urls[0]"
+  group = module.gke[each.key].instance_group_urls[0]
   zone = var.project_region
 
   name = var.router_port_name
