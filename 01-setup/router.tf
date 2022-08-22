f80d257a0f24922ba71bedfc70de8ef77c65e524
Matthew Ratzke <matt@degree9.io>
2022-08-22 09:37:43 -0600

    Update 01-setup/router.tf
    Co-authored-by: Lucas Leadbetter <5595530+lleadbet@users.noreply.github.com>

--- 01-setup/router.tf
+++ 01-setup/router.tf
@@ -4,7 +4,7 @@ resource "google_compute_instance_group_named_port" "router" {
   }
   
   group = "google_container_cluster.${var.demo_name}-${each.value.name}.node_pool[0].instance_group_urls[0]"
-  zone = "us-central1-a"
+  zone = var.project_region
 
   name = var.router_port_name
   port = var.router_port
